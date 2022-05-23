71140f52b5ce4f043302dd671bd56b3999b9e59e
Juuso Ansaharju <juuso@trapridge.me>
2022-05-23 20:06:58 +0300

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Transformer } from "konva/lib/shapes/Transformer";
 import { useRef, useState } from "react";
 import { Calendar } from "./Calendar";
+import styled from "styled-components";
 
 type MonthId =
   | "jan"
@@ -40,8 +40,28 @@ let initialMonths: Months = {
   dec: { id: "dec", color: "#93003a", name: "December", goal: "" },
 };
 
+const Container = styled.section`
+  width: 600px;
+  background-color: palegoldenrod;
+`;
+
+const Form = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 40px 40px 0 40px;
+
+  input {
+    width: 75%;
+  }
+
+  button {
+    margin-left: 10px;
+    width: 25%;
+  }
+`;
+
 const App = () => {
-  const inputRef = useRef<HTMLInputElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null);
   const [goal, setGoal] = useState<string>();
   const [months, setMonths] = useState<Months>(initialMonths);
 
@@ -49,13 +69,15 @@ const App = () => {
     const match = Object.values(months).find(
       (month) => month.goal.length === 0
     );
+
     if (match) {
       setMonths({
         ...months,
         [match.id]: { ...match, goal },
       });
       if (inputRef.current) {
-        inputRef.current.value = ''
+        inputRef.current.value = "";
+        inputRef.current.focus();
       }
     }
   };
@@ -63,6 +85,7 @@ const App = () => {
   const moveGoal = (sourceId: string, targetId: string) => {
     const source = Object.values(months).find((month) => month.id === sourceId);
     const target = Object.values(months).find((month) => month.id === targetId);
+
     if (source && target) {
       setMonths({
         ...months,
@@ -72,20 +95,41 @@ const App = () => {
     }
   };
 
+  const removeGoal = (id: string) => {
+    const month = Object.values(months).find((month) => month.id === id);
+
+    if (month) {
+      setMonths({
+        ...months,
+        [id]: { ...month, goal: "" },
+      });
+    }
+  };
+
+  const isFull = () =>
+    Object.values(months).every((month) => month.goal.length > 0);
+
   return (
-    <>
-      <div style={{ width: "400px", margin: "auto" }}>
+    <Container>
+      <Form>
         <input
           ref={inputRef}
           type="text"
           id="goal"
           name="goal"
           onChange={(e) => setGoal(e.target.value)}
+          onKeyUp={(e) => e.code === "Enter" && addGoal()}
         />
-        <button onClick={addGoal}>Add goal</button>
-      </div>
-      <Calendar months={months} onMoveGoal={moveGoal} />
-    </>
+        <button disabled={isFull()} onClick={addGoal}>
+          Add
+        </button>
+      </Form>
+      <Calendar
+        months={Object.values(months)}
+        onMoveGoal={moveGoal}
+        onRemoveGoal={removeGoal}
+      />
+    </Container>
   );
 };
 
--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,22 +1,12 @@
 import { KonvaEventObject } from "konva/lib/Node";
-import { forwardRef, Fragment, useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import Konva from "konva";
 import { Stage, Layer, Arc, Text, Group } from "react-konva";
-import { Months } from "./App";
+import { Month } from "./App";
 
 const degToRad = (deg: number): number => deg * (Math.PI / 180.0);
+
 const isH2Month = (index: number) => index > 5;
-
-interface CalendarProps {
-  months: Months;
-  onMoveGoal: (source: string, target: string) => void;
-}
-
-const INITIAL_ROTATION = -90;
-const BASE_X = window.innerWidth / 2;
-const BASE_Y = 300;
-const SLICE_ANGLE = 360 / 12;
-const SLICE_OUTER_RADIUS = 200;
 
 interface Dragging {
   id: string;
@@ -24,119 +14,140 @@ interface Dragging {
   startY: number;
 }
 
-export const Calendar = (
-  ({ months, onMoveGoal }: CalendarProps) => {
-    const mainLayerRef = useRef<Konva.Layer>(null);
-    const helperLayerRef = useRef<Konva.Layer>(null);
-    const [draggingGoal, setDraggingGoal] = useState<Dragging | undefined>();
+interface CalendarProps {
+  months: Month[];
+  onMoveGoal: (source: string, target: string) => void;
+  onRemoveGoal: (id: string) => void;
+}
 
-    const startDrag = (e: KonvaEventObject<DragEvent>, id: string) => {
-      if (helperLayerRef) {
-        e.target.moveTo(helperLayerRef.current);
-      }
-      const { x: startX, y: startY } = e.currentTarget.getPosition();
-      setDraggingGoal({ id, startX, startY });
-    };
+const STAGE_SIZE = 600;
+const INITIAL_ROTATION = -90;
+const BASE_X = STAGE_SIZE / 2;
+const BASE_Y = STAGE_SIZE / 2;
+const SLICE_ANGLE = 360 / 12;
+const SLICE_OUTER_RADIUS = STAGE_SIZE / 3;
 
-    const endDrag = (e: KonvaEventObject<DragEvent>) => {
-      const position = e.currentTarget.absolutePosition();
-      if (mainLayerRef.current) {
-        e.currentTarget.moveTo(mainLayerRef.current);
-        const intersection = mainLayerRef.current.getIntersection(position);
-        if (draggingGoal) {
-          if (intersection) {
+export const Calendar = ({
+  months,
+  onMoveGoal,
+  onRemoveGoal,
+}: CalendarProps) => {
+  const mainLayerRef = useRef<Konva.Layer>(null);
+  const helperLayerRef = useRef<Konva.Layer>(null);
+  const [draggingGoal, setDraggingGoal] = useState<Dragging | undefined>();
+
+  const startDrag = (e: KonvaEventObject<DragEvent>, id: string) => {
+    if (helperLayerRef) {
+      e.target.moveTo(helperLayerRef.current);
+    }
+    const { x: startX, y: startY } = e.currentTarget.getPosition();
+    setDraggingGoal({ id, startX, startY });
+  };
+
+  const endDrag = (e: KonvaEventObject<DragEvent>) => {
+    const position = e.currentTarget.absolutePosition();
+    if (mainLayerRef.current) {
+      e.currentTarget.moveTo(mainLayerRef.current);
+      const intersection = mainLayerRef.current.getIntersection(position);
+      if (draggingGoal) {
+        if (intersection) {
+          if (draggingGoal.id !== intersection.attrs.id) {
             onMoveGoal(draggingGoal.id, intersection.attrs.id);
           } else {
+            // intersects with current arc --> reset position
             e.currentTarget.setPosition({
               x: draggingGoal?.startX,
               y: draggingGoal?.startY,
             });
           }
+        } else {
+          // dragged out-of-bounds
+          onRemoveGoal(draggingGoal.id);
         }
       }
-      setDraggingGoal(undefined);
-    };
+    }
+    setDraggingGoal(undefined);
+  };
 
-    const arcs = Object.values(months).map((month, index) => {
-      const TEXT_WIDTH = 100;
-      const TEXT_PADDING = 10;
-      const rotationDeg = INITIAL_ROTATION + index * SLICE_ANGLE;
-
-      return (
-        <Group key={index} x={BASE_X} y={BASE_Y}>
-          <Arc
-            id={month.id}
-            fill={month.color}
-            rotation={rotationDeg}
-            innerRadius={0}
-            outerRadius={SLICE_OUTER_RADIUS}
-            strokeWidth={2}
-            angle={SLICE_ANGLE}
-            stroke="lightgray"
-          />
-          <Text
-            x={
-              Math.cos(degToRad(rotationDeg + SLICE_ANGLE / 2)) *
-              (SLICE_OUTER_RADIUS +
-                TEXT_PADDING -
-                (isH2Month(index) ? -TEXT_WIDTH : 0))
-            }
-            y={
-              Math.sin(degToRad(rotationDeg + SLICE_ANGLE / 2)) *
-              (SLICE_OUTER_RADIUS +
-                TEXT_PADDING -
-                (isH2Month(index) ? -TEXT_WIDTH : 0))
-            }
-            text={month.name}
-            rotation={
-              rotationDeg + SLICE_ANGLE / 2 + (isH2Month(index) ? 180 : 0)
-            }
-            width={TEXT_WIDTH}
-            align={isH2Month(index) ? "right" : "left"}
-          />
-        </Group>
-      );
-    });
-
-    const goals = Object.values(months).map((month, index) => {
-      const PADDING = 120;
-      const rotationDeg =
-        INITIAL_ROTATION + index * SLICE_ANGLE + SLICE_ANGLE / 2;
-      return (
-        month.goal && (
-          <Text
-            id={month.id}
-            name={month.name}
-            ref={(el) => {
-              if (el) {
-                el.offsetX(el.getTextWidth() / 2);
-                el.offsetY(el.getTextHeight() / 2);
-              }
-            }}
-            x={BASE_X + Math.cos(degToRad(rotationDeg)) * PADDING}
-            y={BASE_Y + Math.sin(degToRad(rotationDeg)) * PADDING}
-            text={month.goal}
-            rotation={rotationDeg + (isH2Month(index) ? 180 : 0)}
-            fill="white"
-            draggable={true}
-            key={`${month.id}-${month.goal}`}
-            listening={true}
-            onDragStart={(e) => startDrag(e, month.id)}
-            onDragEnd={endDrag}
-            align="center"
-          />
-        )
-      );
-    });
+  const arcs = months.map((month, index) => {
+    const TEXT_WIDTH = STAGE_SIZE / 6;
+    const TEXT_PADDING = TEXT_WIDTH / 10;
+    const rotationDeg = INITIAL_ROTATION + index * SLICE_ANGLE;
 
     return (
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer ref={mainLayerRef}>
-          {arcs}
-          {goals}
-        </Layer>
-        <Layer ref={helperLayerRef} />
-      </Stage>
+      <Group key={index} x={BASE_X} y={BASE_Y}>
+        <Arc
+          id={month.id}
+          fill={month.color}
+          rotation={rotationDeg}
+          innerRadius={0}
+          outerRadius={SLICE_OUTER_RADIUS}
+          strokeWidth={2}
+          angle={SLICE_ANGLE}
+          stroke="lightgray"
+        />
+        <Text
+          x={
+            Math.cos(degToRad(rotationDeg + SLICE_ANGLE / 2)) *
+            (SLICE_OUTER_RADIUS +
+              TEXT_PADDING -
+              (isH2Month(index) ? -TEXT_WIDTH : 0))
+          }
+          y={
+            Math.sin(degToRad(rotationDeg + SLICE_ANGLE / 2)) *
+            (SLICE_OUTER_RADIUS +
+              TEXT_PADDING -
+              (isH2Month(index) ? -TEXT_WIDTH : 0))
+          }
+          text={month.name}
+          rotation={
+            rotationDeg + SLICE_ANGLE / 2 + (isH2Month(index) ? 180 : 0)
+          }
+          width={TEXT_WIDTH}
+          align={isH2Month(index) ? "right" : "left"}
+        />
+      </Group>
     );
-  }
-);
+  });
+
+  const goals = months.map((month, index) => {
+    const PADDING = STAGE_SIZE / 5;
+    const rotationDeg =
+      INITIAL_ROTATION + index * SLICE_ANGLE + SLICE_ANGLE / 2;
+
+    return (
+      <Text
+        id={month.id}
+        name={month.name}
+        ref={(el) => {
+          if (el) {
+            el.offsetX(el.getTextWidth() / 2);
+            el.offsetY(el.getTextHeight() / 2);
+          }
+        }}
+        x={BASE_X + Math.cos(degToRad(rotationDeg)) * PADDING}
+        y={BASE_Y + Math.sin(degToRad(rotationDeg)) * PADDING}
+        text={month.goal}
+        rotation={rotationDeg + (isH2Month(index) ? 180 : 0)}
+        fill="white"
+        draggable={true}
+        key={`${month.id}-${month.goal}`}
+        listening={true}
+        onDragStart={(e) => startDrag(e, month.id)}
+        onDragEnd={endDrag}
+        align="center"
+        fontSize={14}
+      />
+    );
+  });
+
+  return (
+    <Stage width={STAGE_SIZE} height={STAGE_SIZE}>
+      <Layer ref={mainLayerRef}>
+        {arcs}
+        {goals}
+      </Layer>
+      <Layer ref={helperLayerRef} />
+    </Stage>
+  );
+};
